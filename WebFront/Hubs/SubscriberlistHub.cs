eb1371e8ba0f5f85252fe62eec02a4c4e50ebb1c
Håkan Alexander <hakan.alexander@gmail.com>
2014-05-09 10:27:57 +0200

    First hub

--- WebFront/Hubs/SubscriberlistHub.cs
+++ WebFront/Hubs/SubscriberlistHub.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNet.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.SignalR;
 
 namespace WebFront.Hubs
 {
     public class SubscriberlistHub : Hub
     {
+        private IHubContext _hubs;
+
+        public SubscriberlistHub()
+        {
+            _hubs = GlobalHost.ConnectionManager.GetHubContext<SubscriberlistHub>();
+        }
+
+        public IEnumerable<string> Init()
+        {
+            return new List<string>
+                {
+                    "Kalle",
+                    "Hobbe"
+                };
+        }
     }
 }
