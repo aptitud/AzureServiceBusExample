263f7628daa2744bb0c75b79990e869849fd0536
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2014-05-09 11:03:59 +0200

    Added form functionality

--- WebFront/Bootstrapper.cs
+++ WebFront/Bootstrapper.cs
@@ -1,11 +1,19 @@
 ﻿namespace WebFront
 {
     using Nancy;
+    using Nancy.Conventions;
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
         // The bootstrapper enables you to reconfigure the composition of the framework,
         // by overriding the various methods and properties.
         // For more information https://github.com/NancyFx/Nancy/wiki/Bootstrapper
+
+        protected override void ConfigureConventions(Nancy.Conventions.NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            nancyConventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("/Scripts"));
+        }
     }
 }
--- WebFront/IndexModule.cs
+++ WebFront/IndexModule.cs
@@ -1,6 +1,8 @@
 ﻿namespace WebFront
 {
+    using Microsoft.AspNet.SignalR;
     using Nancy;
+    using WebFront.Hubs;
 
     public class IndexModule : NancyModule
     {
@@ -9,6 +11,14 @@
             Get["/"] = parameters =>
             {
                 return View["index"];
+            };
+
+            Post["/Save"] = parameters =>
+            {
+                string name = Request.Form["Name"];
+                var hub = GlobalHost.ConnectionManager.GetHubContext<SubscriberlistHub>();
+                hub.Clients.All.addNewSubscriber(name);
+                return HttpStatusCode.OK;
             };
         }
     }
