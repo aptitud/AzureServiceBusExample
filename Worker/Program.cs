45115f9f3b1fd02c943e13c04b8d76e1ec487d23
Håkan Alexander <hakan.alexander@gmail.com>
2014-05-09 09:30:26 +0200

    First commit

---
+++ Worker/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Owin.Hosting;
+
+namespace Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:8888/"))
+            {
+                Console.WriteLine("Server running http://localhost:8888/");
+                Console.ReadLine();
+            }
+        }
+    }
+}
